fcf3c5fe53c4b09cf9671d822146b1460bf72a35
ongyongyue <yongyue555@gmail.com>
2026-03-09 19:51:47 +0800

    Finalize

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -12,7 +12,7 @@ import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-
+// Extra Comment
 /**
  * Adds a person to the address book.
  */
